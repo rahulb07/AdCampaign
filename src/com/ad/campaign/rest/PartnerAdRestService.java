65ae80e7df21c89aea1c31420c586b5affdddddb
rahulb07 <rahul7nani@gmail.com>
2016-09-08 00:24:53 -0500

    renaming package name
    renamed package

--- src/com/ad/campaign/rest/PartnerAdRestService.java
+++ src/com/ad/campaign/rest/PartnerAdRestService.java
@@ -1,4 +1,4 @@
-package com.ad.campaign.rest.copy;
+package com.ad.campaign.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
